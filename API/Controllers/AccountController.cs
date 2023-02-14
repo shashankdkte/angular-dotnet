716c3b4fb1d82eed50498288016e37376c0541bd
Shashank Dhakate <shashankdkte@gmail.com>
2023-02-14 14:11:21 +0530

    Added basic login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,6 +38,36 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            //Get user having same username
+            var user = await _context.Users.SingleOrDefaultAsync(
+                x => x.UserName == loginDTO.UserName
+            );
+            //check user exist
+            if (user == null)
+            {
+                return Unauthorized("invalid username");
+            }
+
+            //get password salt
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            //get password hash
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            //check each character
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid Password");
+                }
+            }
+            return user;
+        }
+
         public async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
---
+++ API/DTO/LoginDTO.cs
@@ -0,0 +1,8 @@
+namespace API.DTO
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
